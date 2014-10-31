dbd8edb5db13ba2eb17b44fe1e29a27c786b4e99
Honghao Zhang <zhh358@gmail.com>
2014-10-31 13:45:19 -0400

    works!

--- Playground-iOS/Playground-iOS/ViewController.m
+++ Playground-iOS/Playground-iOS/ViewController.m
@@ -9,7 +9,16 @@
 #import "ViewController.h"
 #import "GDStore.h"
 
+static NSString * STR_KEY = @"demo_string";
+
 @interface ViewController ()
+
+@property (nonatomic, strong) id<GDSStore> store;
+@property (nonatomic, strong) id<GDSDocument> document;
+@property (nonatomic, strong) id<GDSModel> model;
+@property (nonatomic, strong) id<GDSCollaborativeMap> root;
+@property (nonatomic, strong) id<GDSCollaborativeString> collaboratingString;
+@property (nonatomic, strong) NSArray *collaborators;
 
 @end
 
@@ -17,13 +26,20 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    id<GDSStore> store = [[GDSStoreImpl alloc] initWithServerUri:@"ws://realtime.goodow.com:1986/channel/websocket" withOptions:nil];
+    self.textView.textContainerInset = UIEdgeInsetsMake(10, 10, 10, 10);
+    self.textView.text = @"Demo";
+    [self loadDocuments];
+}
+
+- (void)loadDocuments {
+    self.store = [[GDSStoreImpl alloc] initWithServerUri:@"ws://realtime.goodow.com:1986/channel/websocket" withOptions:nil];
     
-    [store load:@"playground/0" onLoaded:^(id<GDSDocument> document) {
-        id<GDSModel> model = [document getModel];
-        id<GDSCollaborativeMap> root = [model getRoot];
-        NSArray *collaborators = [document getCollaborators];
-        
+    [self.store load:@"playground/0" onLoaded:^(id<GDSDocument> document) {
+        self.document = document;
+        self.model = [document getModel];
+        self.root = [self.model getRoot];
+        self.collaborators = [document getCollaborators];
+        [self connectString];
     } opt_initializer:^(id<GDSModel> model) {
         id<GDSCollaborativeMap> root = [model getRoot];
         id<GDSCollaborativeString> string = [model createString:@"Edit Me!"];
@@ -35,6 +51,18 @@
         id<GDSCollaborativeMap> map = [model createMap:@{@"Key 1": @"Value 1", @"Key 2": @"Value 2", @"Key 3": @"Value 3", @"Key 4": @"Value 4"}];
         [root set:@"demo_map" value:map];
     } opt_error:nil];
+}
+
+- (void)connectString {
+    self.collaboratingString = [self.root get:STR_KEY];
+    self.textView.text = [self.collaboratingString getText];
+    [self.collaboratingString onTextInserted:^(id<GDSTextInsertedEvent> insertEvent) {
+        self.textView.text = [self.collaboratingString getText];
+    }];
+    
+    [self.collaboratingString onTextDeleted:^(id<GDSTextDeletedEvent> deleteEvent) {
+        self.textView.text = [self.collaboratingString getText];
+    }];
 }
 
 @end
