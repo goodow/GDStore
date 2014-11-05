7babeb1394433d3d16fd8bf93787f4bca81422a6
Larry Tin <dev@goodow.com>
2014-11-05 14:33:40 +0800

    Update Podfile and Xcode project configurations

--- Project/UnitTest/UnitTest.h
+++ Project/UnitTest/UnitTest.h
@@ -6,8 +6,8 @@
 //  Copyright (c) 2013年 Larry Tin. All rights reserved.
 //
 
-#import <SenTestingKit/SenTestingKit.h>
+#import <XCTest/XCTest.h>
 
-@interface UnitTest : SenTestCase
+@interface UnitTest : XCTestCase
 
 @end
